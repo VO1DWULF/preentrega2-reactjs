acbc337eb284914f07e6a6a016c501fdef05d684
Marco Valencia <mmvb67@gmail.com>
2023-06-12 19:53:27 -0400

    formateando archivos

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -3,17 +3,16 @@ import React from 'react';
 
 const Cart = ({ cart }) => {
     return (
-      <div>
-        <h2>Cart</h2>
-        {cart.map((item) => (
-          <div key={item.id}>
-            <h3>{item.name}</h3>
-            <p>Price: ${item.price}</p>
-            {/* Add any additional information you want to display */}
-          </div>
-        ))}
-      </div>
+        <div>
+            <h2>Cart</h2>
+            {cart.map((item) => (
+                <div key={item.id}>
+                    <h3>{item.name}</h3>
+                    <p>Price: ${item.price}</p>
+                </div>
+            ))}
+        </div>
     );
-  };
-  
+};
+
 export default Cart;
